4dee3a53458f600534944621240a04ff2ae3c550
JanChw <1134806787@qq.com>
2015-09-26 16:59:57 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,23 @@ RSpec.describe "StaticPages", type: :request do
       visit '/'
       expect(page).to have_content('StaticPages#home')
     end
+
+    it "should have the title 'Railstutorial | Home' " do
+      visit '/'
+      expect(page).to have_title("Railstutorial | Home")
+    end
+
   end
 
   describe "help page" do
-    it "should have the content 'StaticPages#https://relishapp' " do
+    it "should have the content 'StaticPages#help' " do
       visit '/help'
       expect(page).to have_content('StaticPages#help')
+    end
+
+    it "should have the content 'Railstutorial | Help'" do
+      visit '/help'
+      expect(page).to have_title("Railstutorial | Help")
     end
   end
 end
