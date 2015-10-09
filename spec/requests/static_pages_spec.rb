c5e4847c3008ca42ae9a48abfc37af012570d508
JanChw <1134806787@qq.com>
2015-10-09 20:11:18 +0800

    改进测试代码

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,39 +13,24 @@ RSpec.describe "StaticPages", type: :request do
   end
 
   describe "help page" do
-    it "should have the content 'StaticPages#help' " do
-      visit help_path
-      expect(page).to have_content('StaticPages#help')
-    end
+    before {visit help_path}
 
-    it "should have the content 'Railstutorial | Help'" do
-      visit '/help'
-      expect(page).to have_title("Railstutorial | Help")
-    end
+    it { should have_content('StaticPages#help')}
+    it { should have_title(full_title('Help'))}
   end
 
   describe "about page" do
-    it "should have the content 'about page'" do
-      visit about_path
-      expect(page).to have_content('About page')
-    end
+    before {visit about_path}
 
-    it "should have the title 'RailsTutotial | About'" do
-      visit about_path
-      expect(page).to have_title('Railstutorial | About')
-    end
+    it { should have_content('About page')}
+    it { should have_title(full_title('About'))}
   end
 
   describe "contact page" do
-    it "should have the content 'contact page'" do
-      visit contact_path
-      expect(page).to have_content('Contact page')
-    end
+    before {visit contact_path}
 
-    it "should have the title 'RailsTutotial | Contact'" do
-      visit contact_path
-      expect(page).to have_title('Railstutorial | Contact')
-    end
+    it { should have_content('Contact page')}
+    it { should have_title(full_title('Contact'))}
   end
 
 end
